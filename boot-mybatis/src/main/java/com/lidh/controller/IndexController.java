a2cdac843c2ba36f0548e1502cdabf5b52887344
lidhk <909827567@qq.com>
2018-07-18 17:16:02 +0800

    整合freemarker模板

---
+++ boot-mybatis/src/main/java/com/lidh/controller/IndexController.java
@@ -0,0 +1,32 @@
+package com.lidh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lidhk on 2018/7/18.
+ *
+ * @author lidhk
+ */
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    @RequestMapping(value = "/login")
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+
+        List<String> userList=new ArrayList<String>();
+        userList.add("admin");
+        userList.add("user1");
+        userList.add("user2");
+
+        modelAndView.addObject("userList", userList);
+        return modelAndView;
+    }
+
+}
